e3d6baa98796c495ba6167286b95fa6aa9ebb167
michal-boop <majzermichal@gmail.com>
2023-03-24 13:46:57 +0100

    change mechanics of grid construction to CSS grid, add button changing it's resolution

--- myscript.js
+++ myscript.js
@@ -1,4 +1,4 @@
-for (let i=0; i<256; i++) { //using loop to create a number of div elements equal to square of input
+/*for (let i=0; i<256; i++) { //using loop to create a number of div elements equal to square of input
     const div = document.createElement('div');
     div.style.border = '1px solid black';
     div.style.minWidth = '28px'; //in this case size of each element is defined to fit the container seamlessly
@@ -8,11 +8,36 @@ for (let i=0; i<256; i++) { //using loop to create a number of div elements equa
     console.log(i); //to make sure the numer of elements checks out
     const grid = document.getElementById('grid'); 
     grid.appendChild(div); //appending each element to the cointainer to make them flex items
+}*/
+getNo = 16;
+gridIt(getNo);
+
+const click = document.getElementById("btn");
+click.onclick = function() {
+    const grid = document.getElementById('grid'); 
+    grid.replaceChildren();
+    let getNo = prompt("choose a number of side blocks up to a 100", "");
+    console.log(getNo);
+    gridIt(getNo);
 }
 
+function gridIt (getNo) {
+    for (let i=1; i<((getNo*getNo)+1); i++) {
+        const div = document.createElement('div');
+        div.classList.add('square'); 
+        console.log(i);
+        const grid = document.getElementById('grid');
+        grid.style.gridTemplate = `repeat(${getNo}, 1fr)/repeat(${getNo}, 1fr)`; 
+        grid.appendChild(div);
+    }
+    colorIt();
+}
+
+function colorIt () {
 const squares = document.getElementsByClassName('square'); 
-for (let i=0; i<squares.length; i++) { //function allowing coloring of the elements on the mouseover
-    squares[i].addEventListener('mouseover', () => {
-        squares[i].style.backgroundColor = 'black';
-    });
+    for (let i=0; i<squares.length; i++) { //function allowing coloring of the elements on the mouseover
+        squares[i].addEventListener('mouseover', () => {
+            squares[i].style.backgroundColor = 'black';
+        });
+    };
 };
